f031278d3b0b5934c1ded3cfc8cd745e8021f10c
Diogo França <dpfs@cin.ufpe.br>
2024-04-22 21:35:33 -0300

    Novas testes.

---
+++ TestesUnitarios.Desafio.Console/Services/OperacoesMatematicas.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestesUnitarios.Desafio.Console.Services
+{
+	public class OperacoesMatematicas
+	{
+		public static int Fibonacci(int n)
+		{
+			return n <= 1 ? n : Fibonacci(n - 1) + Fibonacci(n - 2);
+		}
+
+		public static bool[] CrivodeEratostenes(int upperLimit)
+		{
+			bool[] number = new bool[upperLimit + 1];
+
+			for (int i = 2; i <= upperLimit; i++)
+			{
+				number[i] = true;
+			}
+
+			for (int j = 2; j * j <= upperLimit; j++)
+			{
+				if (number[j] == true)
+				{
+					for (int i = j * j; i <= upperLimit; i += j)
+					{
+						number[i] = false;
+					}
+				}
+			}
+			return number;
+		}
+		public static int AlgoritmoEuclides(int a, int b)
+		{
+			while (b != 0)
+			{
+				int temp = b;
+				b = a % b;
+				a = temp;
+			}
+			return a;
+		}
+
+	}
+}
---
+++ TestesUnitarios.Desafio.Tests/OperacoesMatematicasTests.cs
@@ -0,0 +1,62 @@
+﻿using Xunit;
+using TestesUnitarios.Desafio.Console.Services;
+
+namespace TestesUnitarios.Desafio.Testes
+{
+	public class OperacoesMatematicasTestes
+	{
+		[Theory]
+		[InlineData(0, 0)]
+		[InlineData(1, 1)]
+		[InlineData(2, 1)]
+		[InlineData(5, 5)]
+		[InlineData(10, 55)]
+		public void Fibonacci_DeveRetornarCorretamente(int n, int expected)
+		{
+			// Arrange
+			// Criado pelo InlineData
+			// Act
+			int resultado = OperacoesMatematicas.Fibonacci(n);
+
+			// Assert
+			Assert.Equal(expected, resultado);
+		}
+
+		[Theory]
+		[InlineData(10, 4)] // Até o número 10, há 4 primos (2, 3, 5, 7)
+		[InlineData(20, 8)] // Até o número 20, há 8 primos (2, 3, 5, 7, 11, 13, 17, 19)
+		public void CrivoDeEratostenes_DeveRetornarNumerosPrimosCorretamente(int limite, int esperado)
+		{
+			// Arrange
+
+			// Act
+			bool[] numerosPrimos = OperacoesMatematicas.CrivodeEratostenes(limite);
+			int contadorPrimos = 0;
+			for (int i = 2; i <= limite; i++)
+			{
+				if (numerosPrimos[i])
+				{
+					contadorPrimos++;
+				}
+			}
+
+			// Assert
+			Assert.Equal(esperado, contadorPrimos);
+		}
+
+		[Theory]
+		[InlineData(10, 5, 5)] // MDC de 10 e 5 é 5
+		[InlineData(15, 10, 5)] // MDC de 15 e 10 é 5
+		[InlineData(36, 48, 12)] // MDC de 36 e 48 é 12
+		public void AlgoritmoEuclides_DeveRetornarMDCCorretamente(int a, int b, int esperado)
+		{
+			// Arrange
+
+			// Act
+			int resultado = OperacoesMatematicas.AlgoritmoEuclides(a, b);
+
+			// Assert
+			Assert.Equal(esperado, resultado);
+		}
+	}
+}
